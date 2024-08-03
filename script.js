4be751bc2491dc485e541f2aa7fde5238e036df6
Asma Maria <asantaa31@gmail.com>
2024-08-03 22:03:34 +0600

    fourth commit

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
     const generateKeyBtn = document.getElementById('generateKeyBtn');
     const secretKeyDisplay = document.getElementById('secretKey');
-    const pinInput = document.getElementById('pinInput');
-    const keypadButtons = document.querySelectorAll('.keypad-btn');
     const showMessage = document.getElementById('showMessage');
     let secretKey = '';
+    const Input = document.getElementById('pinInput');
+    const keypadButtons = document.querySelectorAll('.keypad-btn');
 
-    function generateSecretKey() {
+
+
+    function generateKey() {
         secretKey = Math.floor(100000 + Math.random() * 900000).toString();
         secretKeyDisplay.innerHTML = secretKey;
         showMessage.innerHTML = '';
-        pinInput.value = '';
+        Input.value = '';
     }
-    generateKeyBtn.addEventListener('click', generateSecretKey);
+    generateKeyBtn.addEventListener('click', generateKey);
 
     function checkPin() {
-        if (pinInput.value === secretKey) {
+        if (Input.value == secretKey) {
             showMessage.innerHTML = '  <img src="correctIcon.png" alt="check icon" style="width: 15px; height: 15px;"> Your Secret Key is Matched!';
             showMessage.style.color = 'green';
             showMessage.style.fontSize='25px';
             showMessage.style.fontFamily='Times New Roman';
         } else {
-            showMessage.innerHTML = '  <img src="cross.jpg" alt="check icon" style="width: 15px; height: 15px;"> Wrong Secret Key!';
+            showMessage.innerHTML = '  <img src="cross-mark-in-the-red-square-free-vector.jpg" alt="check icon" style="width: 15px; height: 15px;"> Wrong Secret Key!';
             showMessage.style.color = 'red';
             showMessage.style.fontSize='25px';
             showMessage.style.fontFamily='Times New Roman';
@@ -32,16 +34,25 @@ document.addEventListener('DOMContentLoaded', () => {
 
     keypadButtons.forEach(button => {
         button.addEventListener('click', () => {
-            if (button.id === 'clearBtn') {
-                pinInput.value = '';
+
+            if (button.id == 'clearBtn')
+            {
+                Input.value = '';
                 showMessage.textContent = '';
-            } else if (button.id === 'backspaceBtn') {
-                pinInput.value = pinInput.value.slice(0, -1);
-            } else if (button.id === 'submitBtn') {
+            } 
+            else if (button.id == 'backspaceBtn')
+            {
+                Input.value = Input.value.slice(0, -1);
+            } 
+            else if (button.id =='submitBtn') 
+            {
                 checkPin();
-            } else {
-                if (pinInput.value.length < 6) {
-                    pinInput.value += button.textContent;
+            } 
+            else 
+            {
+                if (Input.value.length < 6)
+                {
+                    Input.value = Input.value +  button.textContent;
                 }
             }
         });
